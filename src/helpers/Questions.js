af3bc450b2ce64b56bb4a5a034128a3f1179b1e4
priyanka381 <84623060+priyanka381@users.noreply.github.com>
2021-11-28 12:10:04 +0530

    quiz app

---
+++ src/helpers/Questions.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+      prompt: "What is my name?",
+      optionA: "priya",
+      optionB: "aruna",
+      optionC: "manasa",
+      optionD: "rutuja",
+      asnwer: "priya",
+    },
+    {
+      prompt: "Which of this is not a programming language?",
+      optionA: "Python",
+      optionB: "JavaScript",
+      optionC: "MC-03",
+      optionD: "Java",
+      asnwer: "optionC",
+    },
+    {
+      prompt: "Which of this is not a javascript framework?",
+      optionA: "React",
+      optionB: "Angular",
+      optionC: "Vue",
+      optionD: "Java",
+      asnwer: "optionD",
+    },
+    {
+      prompt: "what is 4*4?",
+      optionA: "12",
+      optionB: "14",
+      optionC: "16",
+      optionD: "20",
+      answer: "optionC",
+},
+  ];
